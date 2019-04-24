329d95e56183a4d7fde3b469942d89490d05a570
hdeqi <hdeqi@qq.com>
2019-04-25 07:58:18 +0800

    add detail

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -17,7 +17,7 @@
       </van-row>
       <van-row style="margin:10px,0px">
           <van-notice-bar
-            text="足协杯战线连续第2年上演广州德比战，上赛季半决赛上恒大以两回合5-3的总比分淘汰富力。"
+            text="足协杯战线连续第2年上演广州德比战，上赛季半决赛上恒大以两回合5-3的总比分淘汰富力。 "
           left-icon="volume-o"
         />
       </van-row>
